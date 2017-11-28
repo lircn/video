a895c23c52f7c3c99f7eb0d1026c54d8bc108f3e
zaynli <zaynli@tencent.com>
2017-11-28 17:32:34 +0800

    update stop

--- dev/stop.sh
+++ dev/stop.sh
@@ -2,11 +2,6 @@
 
 PROG="cap.py"
 
-run=`ps ax|grep "$PROG"|grep -v "grep"`
-if [ "$run" ]; then
-    kill -9 `ps ax|grep "$PROG"|grep -v "grep"|awk '{print $1}'`
-    kill -9 `ps ax|grep "ffmpeg"|grep -v "grep"|awk '{print $1}'`
-    echo "$PROG stopped."
-else
-    echo "$PROG is not running."
-fi
+kill -9 `ps ax|grep "$PROG"|grep -v "grep"|awk '{print $1}'`
+kill -9 `ps ax|grep "ffmpeg"|grep -v "grep"|awk '{print $1}'`
+echo "$PROG stopped."
